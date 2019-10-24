8b2512db548247a248c3903b553bb6499c921967
este <e.tigele@hde.nl>
2019-10-24 23:08:09 +0200

    refactored file structure

---
+++ OurTextAdventure/Models.fs
@@ -0,0 +1,24 @@
+﻿module OurTextAdventure.Models
+
+type Details = { Name: string; Description: string; }
+
+type Item = { Details: Details }
+
+type RoomId = | RoomId of string
+
+type Exit =
+    | PassableExit of Details * destination: RoomId
+    | LockedExit of Details * key: Item * next: Exit
+    | NoExit of Details option
+
+and Exits = { North: Exit; East: Exit; South: Exit; West: Exit; }
+
+and Room = {
+    Id: RoomId
+    Details: Details
+    Items: Item list
+    Exits: Exits }
+
+type Player = { Details: Details; Location: RoomId; Inventory: Item list }
+
+type World = { Rooms: Map<RoomId, Room>; Player: Player }
