36252bcbf760662a635675976a3bfa013f2d1f08
este <e.tigele@hde.nl>
2019-11-13 23:19:11 +0100

    add player details (wip)

---
+++ OurTextAdventure/Features/Players/Details.fs
@@ -0,0 +1,16 @@
+﻿module OurTextAdventure.Features.Players.Details
+
+open OurTextAdventure.Models
+
+type [<CLIMutable>] Query = { PlayerId: PlayerId }
+type Model = { 
+    Id: PlayerId
+    Details: Details
+    Inventory: Item seq
+    Location: RoomId }
+module Handler =  
+    let Handle request = 
+        // get player by id
+        request.PlayerId
+
+        
